7851cb97760cc032fd620c2dc56e78e1d1208574
Frank Goodman <frank_goodman@brown.edu>
2015-04-06 22:16:21 -0400

    started defining core language

--- Redex/phpcore.rkt
+++ Redex/phpcore.rkt
@@ -4,4 +4,28 @@
 
 (provide (all-defined-out))
 
-(define-language lambdaPHP)
+(define-language lambdaPHP
+        (sto ([loc val] ...))
+        (loc natural)
+        (prim boolean number string null)
+        (val prim (lambda (x ...) e) (ref loc))
+        (error (err val))
+        (op + -)
+        (e val
+           error
+           x
+           (op e ...)
+           (e e ...)
+           (let ([x e] ...) e)
+           (set! e e)
+           (begin e e ...)
+           (if e e e)
+           (while e e))
+        (E hole
+           (op val ... E e ...)
+           (val ... E e ...)
+           (let ([x val] ... [y E] [z e] ...) e)
+           (set! E e)
+           (set! val E)
+           (begin val ... E e ...)
+           (if E e e)))
