18c43a3afddfc8dcdb778338dca934508b0ec2ad
Frank Goodman <frank_goodman@brown.edu>
2015-04-08 18:34:57 -0400

    removing let bindings and adding start of global env

--- Redex/phpcore.rkt
+++ Redex/phpcore.rkt
@@ -6,6 +6,7 @@
 
 (define-language 
   lambdaPHP
+  (env ((x loc) ...))
   (sto ((loc val) ...))
   (loc natural)
   (prim boolean number string null)
@@ -13,7 +14,6 @@
   (lbl x)
   (e val
      x
-     (let ([x e] ...) e)
      (set! e e)
      (alloc e)
      (deref e)
@@ -21,14 +21,12 @@
      (label lbl e)
      (break lbl e))
   (H hole
-     (let ([x val] ... [y H] [z e] ...) e)
      (set! H e)
      (set! val H)
      (alloc H)
      (deref H)
      (begin val ... H e ...))
   (E hole
-     (let ([x val] ... [y E] [z e] ...) e)
      (set! E e)
      (set! val E)
      (alloc E)
@@ -59,20 +57,6 @@
    (label any_2 (subst x_1 any_1 e_2))]
    [(subst x_1 any_1 (break any_2 e_2))
    (break any_2 (subst x_1 any_1 e_2))]
-  [(subst y any_1 (let ([x_2 e_2] ...
-                        [y e_y]
-                        [x_3 e_3] ...)
-                    e_4))
-   (let ([x_2 (subst y any_1 e_2)] ...
-         [y (subst y any_1 e_y)]
-         [x_3 (subst y any_1 e_3)] ...)
-     e_4)
-   (side-condition (not (member (term x_1) (term (x_2 ...)))))]
-  [(subst x_1 any_1 (let ([x_2 e_2] ...) e_3))
-   ,(term-let ([(x_new ...) (variables-not-in (term (x_1 any_1 e_3))
-                                              (term (x_2 ...)))])
-              (term (let ([x_new (subst x_1 any_1 e_2)] ...)
-                      (subst x_1 any_1 (subst-vars (x_2 x_new) ... e_3)))))]
   [(subst x_1 any_1 x_1) any_1]
   [(subst x_1 any_1 x_2) x_2]
   [(subst x_1 any_1 any_2) any_2])
@@ -98,9 +82,6 @@
          (((loc_1 val_1) ... (loc val_new) (loc_3 val_3) ...)
           (in-hole E (ref loc)))
          "E-Assign")
-    (==> (let ([x val] ...) e)
-         (subst-n (x val) ... e)
-         "E-Let")
     (--> (sto_1 (in-hole E (alloc val)))
          (sto_2 (in-hole E (ref loc)))
          "E-Alloc"
