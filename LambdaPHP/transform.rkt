c54f639272409b933165a8541f3422aaca2e2137
Frank Goodman <frank_goodman@brown.edu>
2015-04-09 16:03:23 -0400

    function transforms

--- LambdaPHP/transform.rkt
+++ LambdaPHP/transform.rkt
@@ -1,7 +1,33 @@
 #lang racket
 
 (require php-parser)
+(require redex)
 
 (provide (all-defined-out))
 
-(define ex1 (php-parse (open-input-string "<?php $x = 1;?>")))
+(define ex1 (php-parse (open-input-string "<?php
+
+function foo($bar) {
+$x = 1;
+$y = 2;
+}
+
+?>")))
+
+
+(define (parse p)
+  (match p
+    [(? list?) (term (begin ,@(map parse p)))]
+    [(? number?) p]
+    
+    [(Assign _ _ op l r _) 
+     (match op
+       ['ASSIGN `(set ,(parse l) ,(parse r))]
+       [_ (error "unsupported op in Assign")])]
+    [(ExprStmt _ _ e _) (parse e)]
+    [(FunctionDcl _ _ _ n a b _) `(set ,n (lambda () ,(append (parse b) (list null))))]
+    [(ParameterDcl _ _ _ n _ _ _) (string->symbol n)]
+    [(Variable _ _ n _) (string->symbol n)]
+    [_ (error "unsupported expression or statement in parse")]))
+;ex1
+(parse ex1)
